c7fd4c9986c63855c3138cdf797bb33890201b98
null <null>
2025-07-28 16:24:43 -0300

    GameOver Screen: OK, TODO: MainMenu Screen

--- src/scenes/GameOver.js
+++ src/scenes/GameOver.js
@@ -6,10 +6,64 @@ export class GameOver extends Phaser.Scene {
   }
 
   create() {
-    this.youLost = this.add.text(
-      1280 / 2 - 500,
-      720 - 220,
-      'YOU LOST, LOSER!!!'
-    );
+    const gameWidth = this.scale.width;
+    const gameHeight = this.scale.height;
+
+    
+    this.add.rectangle(0, 0, gameWidth, gameHeight, 0x000000, 0.7).setOrigin(0);
+
+   
+    this.add.text(
+      gameWidth / 2, 
+      gameHeight / 4,
+      'Você foi derrotado!',
+      { 
+        fontSize: '64px', 
+        fill: '#ff4d4d', 
+        fontStyle: 'bold',
+        stroke: '#000000',
+        strokeThickness: 4
+      }
+    ).setOrigin(0.5);
+
+    
+    const quitButton = this.add.text(
+      gameWidth / 2, 
+      gameHeight / 2 + 100, 
+      'Desistir', 
+      { 
+        fontSize: '42px', 
+        fill: '#cccccc',
+        fontStyle: 'bold'
+      }
+    )
+      .setOrigin(0.5)
+      .setInteractive({ useHandCursor: true }) 
+      .on('pointerdown', () => {
+        
+        this.scene.start('MainMenu');
+      })
+      .on('pointerover', () => quitButton.setFill('#ff4d4d')) 
+      .on('pointerout', () => quitButton.setFill('#cccccc')); 
+
+    
+    const retryButton = this.add.text(
+      gameWidth / 2, 
+      gameHeight / 2, 
+      'Tentar Novamente', 
+      { 
+        fontSize: '42px', 
+        fill: '#ffffff',
+        fontStyle: 'bold'
+      }
+    )
+      .setOrigin(0.5)
+      .setInteractive({ useHandCursor: true })
+      .on('pointerdown', () => {
+        
+        this.scene.start('Start');
+      })
+      .on('pointerover', () => retryButton.setFill('#FFFF00'))
+      .on('pointerout', () => retryButton.setFill('#ffffff'));
   }
 }
--- src/scenes/Preloader.js
+++ src/scenes/Preloader.js
@@ -94,7 +94,7 @@ export class Preloader extends Phaser.Scene
         })
 
         //Trocar de cena
-        this.scene.start('Start')
+        this.scene.start('GameOver')
     }
 
 }
